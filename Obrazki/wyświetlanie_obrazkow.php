b4af047e758c84df8a81aa215fdf9c902823792b
Karol Lach <hadesto92@gmail.com>
2023-02-24 20:19:32 +0100

    JS

---
+++ Obrazki/wyświetlanie_obrazkow.php
@@ -0,0 +1,7 @@
+<?php
+    echo"<ul>";
+        echo"<li><img src='dziewczyna_anime.jpg'</li>";
+        echo"<li><img src='cosplay.jpg'</li>";
+        echo"<li><img src='duzy.jpg'</li>";
+    echo"</ul>";
+?>
